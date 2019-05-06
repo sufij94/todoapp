9d240ff1c2d34faf5ba38e04e7d92b632c838769
Sufi <sufi.j30@gmail.com>
2019-05-06 18:06:19 -0400

    Added a filter (delete) function
    Now able to filter out completed tasks.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Todos from './Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'learn about react and redux'},
+      {id: 2, content: 'watch game of thrones'}
+    ]
+  }
+
+  deleteToDo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos: todos
+    })
+  }
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos
+          todos={this.state.todos}
+          deleteToDo={this.deleteToDo}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
